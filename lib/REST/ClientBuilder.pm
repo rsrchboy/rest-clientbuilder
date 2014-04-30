0992873f3e1c4f9cb14bb7c95527c9c41757d0b2
Chris Weyl <cweyl@alumni.drew.edu>
2014-04-30 12:34:01 -0700

    initial commit

---
+++ lib/REST/ClientBuilder.pm
@@ -0,0 +1,19 @@
+package REST::ClientBuilder;
+
+# ABSTRACT: The great new REST::ClientBuilder!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
